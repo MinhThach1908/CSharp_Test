f26e531d5f38276165398259449f8c255c992015
MinhThach1908 <pandaminh2008@gmail.com>
2024-07-24 19:40:46 +0700

    Test

--- Exercise_2/SearchingProgram.cs
+++ Exercise_2/SearchingProgram.cs
@@ -2,26 +2,19 @@ namespace CSharp_Test.Exercise_2;
 
 public class SearchingProgram
 {
-    static int Count(string inputString, string searchWord)
-    {
-        int count = 0;
-        int i = 0;
-        while ((i = inputString.IndexOf(searchWord, i, StringComparison.CurrentCultureIgnoreCase)) != 1)
-        {
-            i += searchWord.Length;
-            count++;
-        }
-
-        return count;
-    }
-
     public void DisplayResult()
     {
         Console.WriteLine("Enter a String: ");
         string inputString = Console.ReadLine();
         Console.WriteLine("Enter a word to search");
         string searchWord = Console.ReadLine();
-        int count = Count(inputString, searchWord);
+        int count = 0;
+        int i = 0;
+        while ((i = inputString.IndexOf(searchWord, i, StringComparison.CurrentCultureIgnoreCase)) != -1)
+        {
+            i += searchWord.Length;
+            count++;
+        }
         Console.WriteLine($"Word found {count} times in the string");
     }
 }
--- obj/Debug/net8.0/CSharp_Test.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSharp_Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp_Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f560d9837897fbfb46ebc9784ef850c8d1f8755")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp_Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp_Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
